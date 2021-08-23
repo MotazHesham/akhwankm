2c95acf87902fe17a0490c5a5f386e41d3a51705
Motaz Hesham <61659690+MotazHesham@users.noreply.github.com>
2021-08-23 03:05:33 +0200

    fixings

--- database/migrations/2021_07_26_000025_add_relationship_fields_to_outing_requests_table.php
+++ database/migrations/2021_07_26_000025_add_relationship_fields_to_outing_requests_table.php
@@ -9,7 +9,7 @@ class AddRelationshipFieldsToOutingRequestsTable extends Migration
     public function up()
     {
         Schema::table('outing_requests', function (Blueprint $table) {
-            $table->unsignedBigInteger('outing_type_id');
+            $table->unsignedBigInteger('outing_type_id')->nullable();
             $table->foreign('outing_type_id', 'outing_type_fk_4456305')->references('id')->on('outing_types');
             $table->unsignedBigInteger('big_brother_id');
             $table->foreign('big_brother_id', 'big_brother_fk_4457647')->references('id')->on('big_brothers');
